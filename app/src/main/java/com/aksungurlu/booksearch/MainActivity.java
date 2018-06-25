59a421f33b0abd3f6b746d3fa02f6e3c4148ebde
aksungurlu <t.aksungurlu@gmail.com>
2018-06-26 00:38:25 +0300

    add async activity and refactor url parse

--- app/src/main/java/com/aksungurlu/booksearch/MainActivity.java
+++ app/src/main/java/com/aksungurlu/booksearch/MainActivity.java
@@ -1,6 +1,7 @@
 package com.aksungurlu.booksearch;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,12 +9,15 @@ import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText mSearchEditText;
     private TextView mURLTextView;
+    private TextView mResultsTextView;
     private URL searchURL;
 
     @Override
@@ -25,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
         mSearchEditText = (EditText) findViewById(R.id.et_search);
         //URL TextView
         mURLTextView =(TextView) findViewById(R.id.tv_search_url);
+        //search results TextView
+        mResultsTextView = (TextView) findViewById(R.id.tv_search_results);
     }
 
     @Override
@@ -37,12 +43,40 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId() == R.id.action_search){
             //Toast the edit text
-            Toast.makeText(MainActivity.this, mSearchEditText.getText(), Toast.LENGTH_LONG).show();
+            //Toast.makeText(MainActivity.this, mSearchEditText.getText(), Toast.LENGTH_LONG).show();
             //get parsed URL
-            searchURL = NetworkUtils.buildUrl(mSearchEditText.getText().toString());
-            mURLTextView.setText(searchURL.toString());
+
+            makeQuery(mSearchEditText.getText().toString());
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private void makeQuery(String searchString){
+        searchURL = NetworkUtils.buildUrl(searchString);
+        mURLTextView.setText(searchURL.toString());
+    }
+
+    public class bookQuerry extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchURL = urls[0];
+            String searchResult = null;
+            try{
+                searchResult = NetworkUtils.getResponseFromHttpUrl(searchURL);
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+            return searchResult;
+        }
+
+        @Override
+        protected void onPostExecute(String searchResult) {
+            if(searchResult != null && !searchResult.equals("")){
+                mResultsTextView.setText(searchResult);
+            }
+        }
+    }
+
 }
