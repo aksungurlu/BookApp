e7667923c4ec72f063c0435e3764d73b00316164
aksungurlu <t.aksungurlu@gmail.com>
2018-06-26 00:20:13 +0300

    add scrollview and search result textview to main layout

--- app/src/main/java/com/aksungurlu/booksearch/NetworkUtils.java
+++ app/src/main/java/com/aksungurlu/booksearch/NetworkUtils.java
@@ -2,9 +2,12 @@ package com.aksungurlu.booksearch;
 
 import android.net.Uri;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-
+import java.util.Scanner;
 
 
 public class NetworkUtils {
@@ -26,5 +29,25 @@ public class NetworkUtils {
         }
 
         return url;
+    }
+
+    //copy HTTPURL connection from udacity examples
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
     }
 }
