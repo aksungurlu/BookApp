a3f3b7820abbe8af1cd4d373286146e6f196716c
aksungurlu <t.aksungurlu@gmail.com>
2018-07-07 17:24:08 +0300

    add authors as parsed to a string list

--- app/src/main/java/com/aksungurlu/booksearch/OpenBookJSONUtilities.java
+++ app/src/main/java/com/aksungurlu/booksearch/OpenBookJSONUtilities.java
@@ -4,6 +4,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class OpenBookJSONUtilities {
 
     public static int getBookCount(String jsonString){
@@ -54,12 +57,37 @@ public final class OpenBookJSONUtilities {
                 e.printStackTrace();
             }
             String stringAuthor = null;
+            JSONArray arrayJSONAuthor = null;
+            List<String> authorList = new ArrayList<String>();
             try {
-                stringAuthor = bookJSONArray.getJSONObject(i).getJSONArray(keyAuthor).toString();
+                arrayJSONAuthor = bookJSONArray.getJSONObject(i).getJSONArray(keyAuthor);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-            parsedBookData[i] += " - " + stringAuthor;
+            if(arrayJSONAuthor != null){
+                for(int k = 0; k < arrayJSONAuthor.length(); k++){
+                    try {
+                        authorList.add(arrayJSONAuthor.getString(0));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                parsedBookData[i] += " - Author";
+                if(authorList.size() > 1){
+                    parsedBookData[i] += "s: ";
+                    for(int k = 0; k < authorList.size(); k++){
+                        parsedBookData[i] += authorList.get(k);
+                        if(k < authorList.size() - 1){
+                            parsedBookData[i] += ", ";
+                        }
+                    }
+                }
+                else{
+                    parsedBookData[i] += ": " + authorList.get(0);
+                }
+            }
+
+            //parsedBookData[i] += " - " + stringAuthor;
         }
         return parsedBookData;
     }
